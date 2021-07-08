bfb1fa406462a2df74e0746c1e9759ead154f80b
Thiago Padovani <padovanitr@gmail.com>
2021-07-08 17:38:41 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getUsers, deleteUser } from "./apiMethods";
+
+import "./styles.css";
 
 function App() {
+  const [users, setUsers] = useState(null);
+  const [filteredName, setFilteredName] = useState("");
+
+  const handleGetUsers = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data.data);
+      return data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemoveUser = async (userId) => {
+    const newUsersList = await deleteUser(userId, users);
+
+    setUsers(newUsersList);
+  };
+
+  useEffect(() => {
+    async function getData() {
+      if (filteredName !== "") {
+        let newUsers = await handleGetUsers();
+  
+        if (newUsers) {
+          const newUsersList = newUsers.filter((user) => {
+            return user.first_name === filteredName;
+          });
+          if (newUsersList.length > 0) {
+            setUsers(newUsersList);
+          } else {
+            setUsers(newUsers);
+          }
+        }
+      }
+    }
+
+    getData();
+  }, [filteredName]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Users from API:</h2>
+      <div>
+        <button type="buton" onClick={handleGetUsers}>
+          Click
+        </button>
+
+        <div>
+          <input 
+            type="text"
+            onChange={(e) => setFilteredName(e.target.value)} 
+          />
+        </div>
+
+        {users && (
+          <ul>
+            {users.map((user, index) => (
+              <li key={index} id={user.id}>
+                <img src={user.avatar} alt={user.first_name} />
+                <div className="userInfo">
+                  <p>
+                    {user.first_name} {user.last_name}
+                  </p>
+                  <p>{user.email}</p>
+                  <button onClick={() => handleRemoveUser(user.id)}>
+                    delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/apiMethods.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const baseUrl = "https://reqres.in/";
+
+export const getUsers = async () => {
+  try {
+    let url = baseUrl + "api/users?page=2";
+    const data = await fetch(url);
+    const users = await data.json();
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Bonus:
+export const deleteUser = async (userId, users) => {
+  try {
+    let res = await axios({
+      url: baseUrl + `api/users/${userId}`,
+      method: "get",
+      timeout: 8000,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if (res.status === 200) {
+      const newUsersList = users.filter((user) => {
+        return user.id !== userId;
+      });
+
+      return newUsersList;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
